0c16b29e66598929ecf05969336700247d819544
ChoiChangHwan <ckdghks5565@gmail.com>
2021-03-03 23:11:24 +0900

    add 03 03

--- python/s_01-01.py
+++ python/s_01-01.py
@@ -1,2 +1,14 @@
 # -*- coding: utf-8 -*-
 
+def dict () :
+    
+    x_dict = { }
+
+    key = input("키를 입력하세요 : ")
+    value = input("값을 입력하세요 : ")
+
+    x_dict[key] = value
+    
+    print(x_dict)
+
+dict ()
