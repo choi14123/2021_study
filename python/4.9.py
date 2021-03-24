01f635703af50bc79a8f76decbeb2e2d58f11a1c
최창환 <choichanghwan@choechanghwan-ui-MacBookAir.local>
2021-03-24 22:24:40 +0900

    add day 03 24

---
+++ python/4.9.py
@@ -0,0 +1,17 @@
+import random
+name1 = input("Player 1의 이름을 입력하세요 : ")
+name2 = input("Player 2의 이름을 입력하세요 : ")
+
+name1_score = random.randint(1, 6)
+name2_score = random.randint(1, 6)
+
+print(" ........주사위를 굴립니다....... ")
+print(name1, "주사위 번호는 :",name1_score)
+print(name2, "주사위 번호는 :",name2_score)
+
+if name1_score > name2_score :
+    print(name1, "이 이겼습니다! ")
+elif name1_score < name2_score :
+    print(name2, "이 이겼습니다! ")
+else :
+    print("비겼습니다! ")
