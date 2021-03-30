2f842444f10b8b682cdfc856f0a4f000d77115ed
최창환 <choichanghwan@choechanghwan-ui-MacBookAir.local>
2021-03-30 23:49:43 +0900

    add day 03 30

--- python/Code12-01.py
+++ python/Code12-01.py
@@ -1,12 +1,9 @@
-class Car:
+class  Car :
     color = ""
     speed = 0
 
-    def upSpeed(self, value):
+    def  upSpeed(self, value) :
         self.speed += value
-
-    def downSpeed(self, value):
+    
+    def  downSpeed(self, value) :
         self.speed -= value
-
-    def printMessage():
-        print("시험 출력이다.")
--- python/Code12-04.py
+++ python/Code12-04.py
@@ -1,7 +1,11 @@
-## 클래스 선언 부분 ##
+## 클래스 정의 부분 ##
 class  Car :
     color = ""
     speed = 0
+
+    def __init__(self, value1, value2) :
+        self.color = value1
+        self.speed = value2
 
     def  upSpeed(self, value) :
         self.speed += value
@@ -10,23 +14,9 @@ class  Car :
         self.speed -= value
 
 ## 메인 코드 부분 ##
-myCar1 = Car()
-myCar1.color = "빨강"
-myCar1.speed = 0
+myCar1 = Car("빨강", 30)
+myCar2 = Car("파랑", 60)
 
-myCar2 = Car()
-myCar2.color = "파랑"
-myCar2.speed = 0
-
-myCar3 = Car()
-myCar3.color = "노랑"
-myCar3.speed = 0
-
-myCar1.upSpeed(30)
 print("자동차1의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar1.color, myCar1.speed))
 
-myCar2.upSpeed(60)
 print("자동차2의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar2.color, myCar2.speed))
-
-myCar3.upSpeed(0)
-print("자동차3의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar3.color, myCar3.speed))
