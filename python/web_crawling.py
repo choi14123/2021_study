bd4d57daa2b8f600a6020311c2a7b7de34b4178a
최창환 <ckdghks5565@gmail.com>
2021-05-02 17:26:06 +0900

    add day 05 02

--- python/web_crawling.py
+++ python/web_crawling.py
@@ -1,10 +1,8 @@
-import requests
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-request = requests.get('https://music.bugs.co.kr/chart')
-html = request.text
-soup = BeautifulSoup(html, 'html.parser')
-songs = soup.findAll('p', {'class': 'title'})
-
-for i in range(100):
-    print('{}위 : {} '.format(i + 1, songs[i].text.strip()))
+a = urlopen("https://music.bugs.co.kr/chart")
+soup = BeautifulSoup(a.read(), "html.parser")
+songs = soup.findAll("p", {"class" : "title"})
+for i in range (100) :
+    print("{} 위  :  {}".format(i + 1, songs[i].text.strip()))
