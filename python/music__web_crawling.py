bf33db1265e39a9520858aecac09497db02504fc
최창환 <ckdghks5565@gmail.com>
2021-05-21 23:41:08 +0900

    add day 05 21

--- python/music__web_crawling.py
+++ python/music__web_crawling.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
+import time
 a = urlopen("https://music.bugs.co.kr/chart")
 soup = BeautifulSoup(a.read(), "html.parser")
 songs = soup.findAll("p", {"class" : "title"})
+print(time.strftime('%Y-%m-%d', time.localtime(time.time())),"차트")
 for i in range (100) :
     print("{} 위  :  {}".format(i + 1, songs[i].text.strip()))
