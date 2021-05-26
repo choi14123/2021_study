272aba4e777cdbac059c9c468b38e3946ebed3ab
최창환 <ckdghks5565@gmail.com>
2021-05-26 19:40:54 +0900

    add day 05 26

--- python/music__web_crawling.py
+++ python/music__web_crawling.py
@@ -5,5 +5,5 @@ a = urlopen("https://music.bugs.co.kr/chart")
 soup = BeautifulSoup(a.read(), "html.parser")
 songs = soup.findAll("p", {"class" : "title"})
 print(time.strftime('%Y-%m-%d', time.localtime(time.time())),"차트")
-for i in range (100) :
+for i in range (50) :
     print("{} 위  :  {}".format(i + 1, songs[i].text.strip()))
