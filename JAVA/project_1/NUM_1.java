7395d3fc81acb37064784e8ea676922c88f20c7b
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-06-30 13:20:20 +0900

    add day 06 30

---
+++ JAVA/project_1/NUM_1.java
@@ -0,0 +1,6 @@
+public class NUM_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ JAVA/project_1/NUM_3.java
@@ -0,0 +1,8 @@
+public class NUM_3 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
